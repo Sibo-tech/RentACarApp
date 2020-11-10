ddad5451d473cafd0942fae837612adabdd15bf1
Sibo-tech <sibongile.maseko074@gmail.com>
2020-11-10 12:41:16 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+var modals = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+document.getElementById("list").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("index.html").classList.toggle("show");
+}
